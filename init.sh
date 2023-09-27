60276a1f2d5b722e04daf735e8816986241a4ac4
Helder Davidson Rodrigues Alvarenga <70406114+Uken49@users.noreply.github.com>
2023-09-27 18:02:12 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,9 +2,8 @@
 
 sudo apt update -y && sudo apt upgrade -y
 
-sudo passwd ubuntu
-
 sudo apt install docker.io -y
+sudo apt install docker-compose -y
 
 sudo systemctl start docker
 sudo systemctl enable docker
